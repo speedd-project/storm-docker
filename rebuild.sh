7ed65835846eeb173d5281e50d05c9d111446837
Alexander Kofman <KOFMAN@il.ibm.com>
2016-01-04 10:00:02 +0200

    some windows-related fixes

--- rebuild.sh
+++ rebuild.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t="wurstmeister/storm" storm
+docker build -t="wurstmeister/storm:0.9.4" storm
 docker build -t="wurstmeister/storm-nimbus" storm-nimbus
 docker build -t="wurstmeister/storm-supervisor" storm-supervisor
 docker build -t="wurstmeister/storm-ui" storm-ui
